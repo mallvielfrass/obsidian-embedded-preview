85f02cf7760cfe739279a080ae9e48db2430ad7f
mallvielfrass <jack@bykollab.com>
2024-01-03 01:11:20 +0300

    add  obsidian request instead fetch(fixing cors)

--- preview/preview.ts
+++ preview/preview.ts
@@ -1,4 +1,5 @@
-import { getLinkPreview } from "link-preview-js";
+import { getPreviewFromContent } from "link-preview-js";
+import { request } from "obsidian";
 
 export interface IPreview {
   url: string;
@@ -12,12 +13,36 @@ export async function urlPreview(
   url: string,
   timeout = 30000
 ): Promise<IPreview> {
-  const resp: any = await new Promise((resolve, reject) => {
+  //   const respBody = await fetch(url, {
+  //     mode: "no-cors",
+  //   })
+  //     .then((response) => {
+  //       console.log("fetch", response);
+  //     })
+  //     .catch((error) => {
+  //       console.log("error", error);
+  //     });
+  //   console.log("respBody", respBody);
+  const resp: any = await new Promise(async (resolve, reject) => {
     const timeO = setTimeout(() => {
       reject(new Error("превышено время ожидания ответа"));
     }, timeout);
-
-    getLinkPreview(url)
+    // console.log("urlPreview>", url);
+    const myRequest = await request(url);
+    // console.log("myRequest", myRequest);
+    const resource: {
+      headers: Record<string, string>;
+      status?: number;
+      imagesPropertyType?: string;
+      proxyUrl?: string;
+      url: string;
+      data: string;
+    } = {
+      headers: {},
+      url: url,
+      data: myRequest,
+    };
+    getPreviewFromContent(resource)
       .then(async (result) => {
         clearTimeout(timeO);
         resolve(result);
