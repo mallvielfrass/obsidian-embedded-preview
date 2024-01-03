ee2bcb709979d15307fa4b78522315d829d1577c
mallvielfrass <jack@bykollab.com>
2024-01-03 17:35:55 +0300

    todo fix broken obsidian tests

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,14 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  verbose: true,
   testMatch: [
     "**/__tests__/**/*.+(ts|tsx|js)",
     "**/?(*.)+(spec|test).+(ts|tsx|js)",
   ],
+  moduleNameMapper: {
+    "^obsidian$": "node_modules/obsidian",
+  },
+
+  modulePathIgnorePatterns: ["node_modules/obsidian"],
 };
